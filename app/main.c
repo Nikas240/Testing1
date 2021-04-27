bd71b3149835a0f51fb864d899eb2140caf82e94
Nikas240 <61223884+Nikas240@users.noreply.github.com>
2021-04-27 17:04:29 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -6,13 +6,6 @@
 
 int main()
 {
-    values val;
-    double a;
-    double b;
-    double c;
-    printf("Enter \n");
-    scanf("%lf %lf %lf", &a, &b, &c);
-    val = newfunc(a, b, c);
-    if (val.flag == 2) printf("Solved: x1 = %.2lf, x2 = %.2lf \n", val.x1, val.x2);
-    return 0;
+    printf("Hello World!\n");
+    return myfunc(2);
 }
